134794081c21602215d1befe21502dd3775fd7c2
Kevin Claytor <KE.Claytor@gmail.com>
2021-02-06 21:40:54 -0500

    Color designer implemented.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource
-from bokeh.models import ColorPicker, TextInput
+from bokeh.models import Panel, Tabs
+from bokeh.models import TextInput, ColorPicker, Button
 from bokeh.io import curdoc
 from bokeh.layouts import column
 from bokeh.events import DoubleTap, Tap
@@ -20,33 +21,61 @@ y0 = np.array(y0)
 ys = np.array(ys)
 selected = np.zeros(x0.shape, dtype=bool)
 
-# UI elements
-text_selected_index = TextInput(value="[]", title="Selected Index:")
+# UI elements - Character designer
+char_text = TextInput(value="[]", title="Selected Index:")
+
+# UI elements - Color picker
+color_button = Button(label="Reset")
+color_text = TextInput(value="[]", title="Colors:")
+color_picker = ColorPicker(title="Color")
 
 # Plot
 p = figure(plot_width=600, plot_height=400, x_range=(-6, 6), y_range=(-4, 4),)
 
 p.patches(xs="xs", ys="ys", fill_color="colors", source=source)
 
+# Reset the array
+def reset(event):
+    colors = np.array([""] * len(x0), dtype=object)
+    colors[:] = "black"
+    source.data["colors"] = colors.tolist()
+
+
 # Switch the colors of the clicked location
 def callback(event):
     indx = np.argmin((x0 - event.x) ** 2 + (y0 - event.y) ** 2)
-    selected[indx] = not selected[indx]
-    colors = np.array([""] * len(x0), dtype=object)
-    colors[~selected] = "black"
-    colors[selected] = "orange"
-    print(colors)
+    if tabs.active == 0:
+        # ==== Index Designer ====
+        selected[indx] = not selected[indx]
+        colors = np.array([""] * len(x0), dtype=object)
+        colors[~selected] = "black"
+        colors[selected] = "orange"
+        # Update the UI
+        char_text.value = str(np.where(selected)[0].tolist())
+    elif tabs.active == 1:
+        # ==== Color Designer ====
+        colors = np.array(source.data["colors"], dtype=object)
+        colors[indx] = color_picker.color
+        # Update the UI
+        color_text.value = str(colors.tolist())
     # We have to replace the data to refresh the plot
     # There may be a more efficient "patch" method, but it's not working for me
     source.data["colors"] = colors.tolist()
-    # Update the UI
-    text_selected_index.value = str(np.where(selected)[0].tolist())
 
 
 p.on_event(Tap, callback)
+color_button.on_click(reset)
 
-ui = column([text_selected_index])
+ui = column([char_text])
 
-layout = column([ui, p])
+layout_char = column([char_text])
+tab_char_designer = Panel(child=layout_char, title="Index Designer")
+
+layout_color = column([color_button, color_text, color_picker])
+tab_color_designer = Panel(child=layout_color, title="Color Designer")
+
+tabs = Tabs(tabs=[tab_char_designer, tab_color_designer])
+
+layout = column([tabs, p])
 
 curdoc().add_root(layout)

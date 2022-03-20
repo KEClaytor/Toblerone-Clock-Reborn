9a35836ed3539d9a30930a60369bdc0cb168b232
Kevin Claytor <KE.Claytor@gmail.com>
2022-03-20 12:06:32 -0400

    Fix to hardware implementation.

--- hardware.py
+++ hardware.py
@@ -9,21 +9,17 @@ try:
 
     # NeoPixels must be connected to D10, D12, D18 or D21 to work.
     pixel_pin = board.D18
-    num_pixels = 125
+    num_pixels = 126
 
-    # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
-    # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
-    ORDER = neopixel.RGB
-
-    pixels = neopixel.NeoPixel(
-        pixel_pin, num_pixels, brightness=0.5, auto_write=False, pixel_order=ORDER
-    )
-
-
-    def hex_to_rgb(str):
-        """Convert hex string to (r,g,b) tuple.
-        """
-        return tuple(int(str[ii:ii+2], 16) for ii in (0, 2, 4))
+    pixels = neopixel.NeoPixel(pixel_pin, num_pixels, auto_write=False)
 
 except ModuleNotFoundError:
     pixels = None
+
+
+def hex_to_rgb(chex):
+    """Convert color hex string to (r,g,b) tuple.
+    """
+    chex = chex.lstrip("#")
+    return tuple(int(chex[ii:ii+2], 16) for ii in (0, 2, 4))
+
